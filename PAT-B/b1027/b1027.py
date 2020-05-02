599533b4da053476f5fad9bf5bbb41cee5c638ef
zhangpeng96 <sjiaomail@gmail.com>
2020-05-02 22:19:00 +0800

    b1027，测试点未通过

--- PAT-B/b1027/b1027.py
+++ PAT-B/b1027/b1027.py
@@ -1,11 +1,22 @@
-import math
+def star_count(rank):
+    return 1 + sum([ 2*i + 1 for i in range(1, rank)]) * 2
 
-def find_nearest(num):
-	if num < 1:
-		return 0
-	elif num < 7:
-		return 1
-	else:
-		return math.floor((num + 1) / 4)
+def calc_rank(given):
+    for i in range(1, 23):
+        if star_count(i) > 19:
+            return i-1
 
-def draw(n):
+def star_draw(rank, star):
+    # star = '*'
+    star_part = [''.join([star] * (2*i+1)) for i in range(1, rank)]
+    star_list = star_part[::-1] + [star] + star_part
+    width_format = r'{:^' + str(len(star_list[0])) + r'}'
+    print('\n'.join(map(lambda x: width_format.format(x), star_list)))
+
+
+if __name__ == '__main__':
+    given, star = '19 *'.split()
+    given = int(given)
+    rank = calc_rank(given)
+    star_draw(rank, star)
+    print(given - star_count(rank))
