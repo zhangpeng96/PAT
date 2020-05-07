64b4376e53519818a7fb0068b8dd4b7a48ca77db
zhangpeng96 <sjiaomail@gmail.com>
2020-05-07 17:07:49 +0800

    b1036，全部测试点通过

--- PAT-B/b1036/python-1036.py
+++ PAT-B/b1036/python-1036.py
@@ -1,15 +1,16 @@
 '''
     @name      : b1036
-    @version   : 20.507
+    @version   : 20.507.2
     @author    : zhangpeng96
-    @test_time : 
-    @pass_rate : p2 failed
+    @test_time : 20'22"
+    @pass_rate : all
 '''
 
 # input_str = '10 a'
 input_str = input()
 count, token = int(input_str.split()[0]), input_str.split()[1]
-width, height = count, int(count/2)
+width, height = count, round((count+10e-7)/2)
+# 题目中要求采用四舍五入处理，python3中round(9/4)为4，而非5
 
 draw = [token*count] + [token + ' '*(width-2) + token]*(height-2) + [token*count]
 
