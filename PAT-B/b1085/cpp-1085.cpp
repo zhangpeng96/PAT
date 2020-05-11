e43cc98b5f4b0e21c969269f6430115870b24b9f
zhangpeng96 <sjiaomail@gmail.com>
2020-05-11 20:01:29 +0800

    b1085，C++全部测试点通过，耗时509/800ms

--- PAT-B/b1085/cpp-1085.cpp
+++ PAT-B/b1085/cpp-1085.cpp
@@ -1,8 +1,8 @@
 /*
     @name      : b1085
-    @version   : 20.0511
+    @version   : 20.0511.2
     @author    : zhangpeng96
-    @test_time : 
+    @test_time : 223'40"
     @pass_rate : all
 */
 
@@ -15,36 +15,77 @@
 using namespace std;
 
 struct node {
-	float level;
-	int count, score;
     string school;
-    string id;
-    node() {
-    	count = 0;
-    	score = 0;
-	}
+    int score;
+    int count;
 };
 
+struct data {
+    float score;
+    int count;
+};
+
+bool cmp(node a, node b) {
+    if (a.score != b.score) {
+        return a.score > b.score;
+    }
+    if (a.count != b.count) {
+        return a.count < b.count;
+    }
+    return a.school < b.school;
+}
+
+
 int main() {
-	ios::sync_with_stdio(false);
-	map<char, float> grade_weight;
-	grade_weight['A'] = 1;
-	grade_weight['B'] = 1/1.5;
-	grade_weight['T'] = 1.5;
-//	printf("%.2f", grade_weight['B']);
-//	for (int i = 0; i < 2; i++) {
-		node temp;
-		int score;
-		cin >> temp.id >> score >> temp.school;
-//		transform(school.begin(), school.end(), school.begin(), ::tolower);
-		transform(temp.school.begin(), temp.school.end(), temp.school.begin(), ::tolower);
-//		temp.school = school;
-		temp.level = grade_weight[temp.id[0]];
-		temp.score += score;
-//		cout << school[0] << endl;
-		cout << temp.school << " " << temp.level << " " << temp.score << endl; 
-//	}
-//	vector<node> admission;
-	return 0;
+    ios::sync_with_stdio(false);
+//	freopen("test_point.txt", "r", stdin);
+
+    vector<node> nodes;
+    map<string, data> item;
+    map<char, float> grade_weight;
+    grade_weight['A'] = 1;
+    grade_weight['B'] = 1/1.5;
+    grade_weight['T'] = 1.5;
+
+    int count;
+    cin >> count;
+
+    for (int i = 0; i < count; i++) {
+        string school, pass_id;
+        float score;
+
+        cin >> pass_id >> score >> school;
+        score *= grade_weight[pass_id[0]];
+        transform(school.begin(), school.end(), school.begin(), ::tolower);
+
+        if (item.count(school)) {
+            item[school].score += score;
+            item[school].count ++;
+        } else {
+            item.insert(make_pair(school, data {score, 1}));
+        }
+    }
+
+    for (auto &x : item) {
+        nodes.push_back(node {x.first, (int)x.second.score, x.second.count});
+    }
+    sort(nodes.begin(), nodes.end(), cmp);
+
+    int prev_score = -1;
+    int order, rank;
+    order = 0;
+    rank = 0;
+
+    cout << nodes.size() << endl;
+    for (auto &x: nodes) {
+        order ++;
+        if (prev_score != x.score) {
+            rank = order;
+        }
+        prev_score = x.score;
+        cout << rank << " " << x.school << " " << x.score << " " << x.count << endl;
+    }
+
+    return 0;
 }
 
