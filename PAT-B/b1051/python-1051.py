bec5610d0140c690e9deeae291a89d1977c12061
zhangpeng96 <sjiaomail@gmail.com>
2020-05-13 20:46:44 +0800

    b1051, 测试通过，ieee负0问题

--- PAT-B/b1051/python-1051.py
+++ PAT-B/b1051/python-1051.py
@@ -1,9 +1,9 @@
 '''
     @name      : b1051
-    @version   : 20.013
+    @version   : 20.0513.2
     @author    : zhangpeng96
-    @test_time : 21'15"
-    @pass_rate : p3, p4 failed
+    @test_time : 26'10"
+    @pass_rate : all
 '''
 from math import pi, sin, cos, radians
 
@@ -15,11 +15,21 @@ def complex_mul(c1, c2):
     r2, i2 = c2
     return tuple([r1*r2 - i1*i2, r1*i2 + i1*r2])
 
-r1, p1, r2, p2 = tuple(map(float, '2.3 0 0 0'.split()))
+def round_nz(compl, prec = 0):
+    return tuple(map(lambda x: round(x, prec) + 0, compl))
+
+# r1, p1, r2, p2 = tuple(map(float, '-0.1 -0.01 5.5 5.5'.split()))
 # r1, p1, r2, p2 = tuple(map(float, '2.3 3.5 5.2 0.4'.split()))
-# r1, p1, r2, p2 = tuple(map(float, input().split()))
+r1, p1, r2, p2 = tuple(map(float, input().split()))
 
 c1 = to_algebra(r1, p1)
 c2 = to_algebra(r2, p2)
 
-print('{:.2f}{:+.2f}i'.format(*complex_mul(c1, c2)))
+print('{:.2f}{:+.2f}i'.format(* round_nz(complex_mul(c1, c2), 2) ))
+
+
+'''
+    References
+    - https://stackoverflow.com/questions/11010683/how-to-have-negative-zero-always-formatted-as-positive-zero-in-a-python-string
+    - https://stackoverflow.com/questions/4083401/negative-zero-in-python
+'''
