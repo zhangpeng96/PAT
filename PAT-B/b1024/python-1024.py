23d8d574ece8ebad91757d0bde7fb25eb9ef3c21
zhangpeng96 <sjiaomail@gmail.com>
2020-05-14 21:00:55 +0800

    b1024，全部通过，只需要在20.0514版本中把eval改为Decimal，使用定点小数保留精确度

--- PAT-B/b1024/python-1024.py
+++ PAT-B/b1024/python-1024.py
@@ -1,36 +1,32 @@
 '''
     @name      : b1024
-    @version   : 20.0514.2
+    @version   : 20.0514.3
     @author    : zhangpeng96
-    @test_time : 32'58"
-    @pass_rate : p5, p6 failed
+    @test_time : 83'12"
+    @pass_rate : all
 '''
+from decimal import Decimal
 
 def decimal_format(deci):
     deci_ns = deci.lstrip('+').lstrip('-')
     if deci_ns.count('-'):
         deci_count = len(deci_ns.split('E')[0].split('.')[1]) + int(deci_ns.split('-')[1])
-        return ('{:.' + str(deci_count) + 'f}').format(eval(deci))
+        return '{:.' + str(deci_count) + 'f}'
     else:
         int_count = int(deci_ns.split('+')[1])
         deci_count = len(deci_ns.split('E')[0].split('.')[1])
-        if int_count <= 99:
-            float_count = 0 if deci_count <= int_count else deci_count - int_count
-            return ('{:.' + str(float_count) + 'f}').format(eval(deci))
-        else:
-            deci = deci.split('E')[0] + 'E+99'
-            int_count -= 99
-            float_count = 0 if deci_count <= int_count else deci_count - int_count
-            return ('{:.' + str(float_count) + 'f}').format(eval(deci))
+        float_count = 0 if deci_count <= int_count else deci_count - int_count
+        return '{:.' + str(float_count) + 'f}'
+
+input_str = input()
+print( decimal_format(input_str).format(Decimal(input_str)) )
 
 
-# input_str = input()
-# print( decimal_format(input_str).format(eval(input_str)) )
+# input_1 = '+1.23400E-03'
+# print(decimal_format(input_1).format(Decimal(input_1)))
 
+# input_2 = '-1.2E+10'
+# print(decimal_format(input_2).format(Decimal(input_2)))
 
-# input_1 = '+1.23400E-00'
-# print(decimal_format(input_1))
-
-input_2 = '-0.01E+90'
-print(decimal_format(input_2))
-# -9999999999999999594167244563503627314919960896484514396697390098067039229509544255160320
+# input_3 = '-1.0001E+999'
+# print(decimal_format(input_3).format(Decimal(input_3)))
