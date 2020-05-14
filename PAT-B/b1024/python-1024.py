5c46b9e36cba4fb0257627baaf206b88c71a21d7
zhangpeng96 <sjiaomail@gmail.com>
2020-05-14 20:54:29 +0800

    b1024，修改失败，发现是浮点数精确度问题

--- PAT-B/b1024/python-1024.py
+++ PAT-B/b1024/python-1024.py
@@ -1,6 +1,6 @@
 '''
     @name      : b1024
-    @version   : 20.0514
+    @version   : 20.0514.2
     @author    : zhangpeng96
     @test_time : 32'58"
     @pass_rate : p5, p6 failed
@@ -10,19 +10,27 @@ def decimal_format(deci):
     deci_ns = deci.lstrip('+').lstrip('-')
     if deci_ns.count('-'):
         deci_count = len(deci_ns.split('E')[0].split('.')[1]) + int(deci_ns.split('-')[1])
-        return '{:.' + str(deci_count) + 'f}'
+        return ('{:.' + str(deci_count) + 'f}').format(eval(deci))
     else:
         int_count = int(deci_ns.split('+')[1])
         deci_count = len(deci_ns.split('E')[0].split('.')[1])
-        float_count = 0 if deci_count <= int_count else deci_count - int_count
-        return '{:.' + str(float_count) + 'f}'
+        if int_count <= 99:
+            float_count = 0 if deci_count <= int_count else deci_count - int_count
+            return ('{:.' + str(float_count) + 'f}').format(eval(deci))
+        else:
+            deci = deci.split('E')[0] + 'E+99'
+            int_count -= 99
+            float_count = 0 if deci_count <= int_count else deci_count - int_count
+            return ('{:.' + str(float_count) + 'f}').format(eval(deci))
+
 
 # input_str = input()
 # print( decimal_format(input_str).format(eval(input_str)) )
 
 
-input_1 = '+1.23400E-03'
-print(decimal_format(input_1).format(eval(input_1)))
+# input_1 = '+1.23400E-00'
+# print(decimal_format(input_1))
 
-input_2 = '-1.2E+10'
-print(decimal_format(input_2).format(eval(input_2)))
+input_2 = '-0.01E+90'
+print(decimal_format(input_2))
+# -9999999999999999594167244563503627314919960896484514396697390098067039229509544255160320
