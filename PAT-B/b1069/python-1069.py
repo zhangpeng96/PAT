4109a27dbc6d5d5da388a24d81afe1af49ba5a1e
zhangpeng96 <sjiaomail@gmail.com>
2020-05-16 18:45:28 +0800

    b1069, 测试全部通过，理解题目有误，若账号有连续相同，则都视为一个账号

--- PAT-B/b1069/python-1069.py
+++ PAT-B/b1069/python-1069.py
@@ -1,28 +1,25 @@
 '''
     @name      : b1069
-    @version   : 20.0515
+    @version   : 20.0516
     @author    : zhangpeng96
-    @test_time : 
-    @pass_rate : p0, p3 failed
+    @test_time : 22'00"
+    @pass_rate : all
 '''
 
-count, gap, start = tuple(map(int, '9 3 2'.split()))
+# count, gap, start = tuple(map(int, '9 3 2'.split()))
+# accounts = ['Imgonnawin!','PickMe','PickMeMeMeee','LookHere','Imgonnawin!','TryAgainAgain','TryAgainAgain','Imgonnawin!','TryAgainAgain']
+# accounts = ['PickMe','Imgonnawin!','TryAgainAgain']
 
-accounts = ['Imgonnawin!','PickMe','PickMeMeMeee','LookHere','Imgonnawin!','TryAgainAgain','TryAgainAgain','Imgonnawin!','TryAgainAgain']
-accounts = ['PickMe','Imgonnawin!','TryAgainAgain']
-
-
-# count, gap, start = tuple(map(int, input().split()))
-# accounts = [input() for _ in range(count)]
+count, gap, start = tuple(map(int, input().split()))
+accounts = [input() for _ in range(count)]
 
 selected = []
-
-for i, ac in enumerate(accounts[start-1::gap]):
-    if ac in selected:
-        selected.append(accounts[i+1])
-    else:
-        selected.append(ac)
-
+count = 0
+for account in accounts[start-1:]:
+    if account not in selected:
+        if not count % gap:
+            selected.append(account)        
+        count += 1
 
 if selected:
     print('\n'.join(selected))
