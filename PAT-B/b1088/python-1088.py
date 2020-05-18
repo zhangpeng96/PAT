704644ecd44ac8bb884b962d253a638ab2fb27cb
zhangpeng96 <sjiaomail@gmail.com>
2020-05-18 10:52:03 +0800

    b1088, 全部通过，读题错误，输出结果第一项是推测出的甲，而非输入的值

--- PAT-B/b1088/python-1088.py
+++ PAT-B/b1088/python-1088.py
@@ -2,8 +2,8 @@
     @name      : b1088
     @version   : 20.0518
     @author    : zhangpeng96
-    @test_time : 26'32"
-    @pass_rate : p0, p2, p3, p4, p5 failed
+    @test_time : 83'53"
+    @pass_rate : all
 '''
 
 def equation(x, y):
@@ -11,7 +11,7 @@ def equation(x, y):
     for a in range(10, 100):
         b = int(str(a)[::-1])
         if x * b == abs(a - b) * y:
-            solution = [a, b, b//y]
+            solution = [a, b, b/y]
     return solution
 
 def decision(me, other):
@@ -23,12 +23,14 @@ def decision(me, other):
         return 'Gai'
 
 
-me, x, y = tuple(map(int, '48 3 7'.split()))
-# me, x, y = tuple(map(int, input().split()))
+# me, x, y = tuple(map(int, '48 3 7'.split()))
+inputs = input()
+me, x, y = tuple(map(int, inputs.split()))
+
 solution = equation(x, y)
 if solution:
     print('{} {} {} {}'.format(
-        me, *[decision(me, p) for p in solution]
+        solution[0], *[decision(me, p) for p in solution]
     ))
 else:
     print('No Solution')
