2b4ef517bc2ade0647b66406a4e95aed538eb806
zhangpeng96 <sjiaomail@gmail.com>
2020-05-20 17:20:10 +0800

    b1014, 全部通过，p1,p2是decode_hour未对字符内容判断，p4是decode_week未对字符内容判断，另外hour需要前导零

--- PAT-B/b1014/python-1014.py
+++ PAT-B/b1014/python-1014.py
@@ -3,12 +3,12 @@
     @version   : 20.0520
     @author    : zhangpeng96
     @test_time : 45'00"
-    @pass_rate : p1,p2,p4 failed
+    @pass_rate : all
 '''
 
 def decode_week(str1, str2, pos = False):
     for i in range(min(len(str1), len(str2))):
-        if str1[i] == str2[i] and str1[i].isupper():
+        if str1[i] == str2[i] and str1[i] in 'ABCDEFG':
             if pos:
                 return i
             return ord(str1[i]) - 64
@@ -17,7 +17,7 @@ def decode_hour(str1, str2):
     pos = decode_week(str1, str2, True) + 1
     str1, str2 = str1[pos:], str2[pos:]
     for i in range(min(len(str1), len(str2))):
-        if str1[i] == str2[i]:
+        if str1[i] == str2[i] and str1[i] in '0123456789ABCDEFGHIJKLMN':
                 return str1[i]
 
 def decode_min(str1, str2):
@@ -35,10 +35,9 @@ input_str = [
 
 format_weeks = ['', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 format_hours = dict(zip('0123456789ABCDEFGHIJKLMN', range(24)))
-format_mins = lambda x: '{:02d}'.format(x)
 
-print('{} {}:{}'.format(
+print('{} {:02d}:{:02d}'.format(
     format_weeks[ decode_week(input_str[0], input_str[1]) ],
     format_hours[ decode_hour(input_str[0], input_str[1]) ],
-    format_mins( decode_min(input_str[2], input_str[3]) ),
+    decode_min(input_str[2], input_str[3]),
 ))
