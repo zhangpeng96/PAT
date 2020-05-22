68ed3663a8ffeadd93ceea1d7d5a7eab94c1ab14
zhangpeng96 <sjiaomail@gmail.com>
2020-05-22 15:35:51 +0800

    b1070, p1 failed

---
+++ PAT-B/b1070/python-1070.py
@@ -0,0 +1,20 @@
+'''
+    @name      : b1070
+    @version   : 20.0522
+    @author    : zhangpeng96
+    @test_time : 
+    @pass_rate : p1 failed
+'''
+
+count = int(input())
+ropes = tuple(sorted(map(int, input().split())))
+
+# ropes = tuple(sorted(map(int, '10 15 12 3 4 13 1 15'.split())))
+
+rope_length = ropes[0] // 2
+
+for r in ropes[1:]:
+    rope_length = (rope_length + r) // 2
+
+print(rope_length)
+
