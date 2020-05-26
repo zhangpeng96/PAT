70e5e34bc9523d03bb5dcacce4060beaf906d97a
zhangpeng96 <sjiaomail@gmail.com>
2020-05-26 18:13:37 +0800

    b1010, 全部测试点通过，题目理解不对

--- PAT-B/b1010/python-1010.py
+++ PAT-B/b1010/python-1010.py
@@ -2,22 +2,22 @@
     @name      : b1010
     @version   : 20.0526
     @author    : zhangpeng96
-    @test_time : 
-    @pass_rate : p2, p4 failed
+    @test_time : 38'48"
+    @pass_rate : all
 '''
 
-items = tuple(map(int, '3 4 -5 2 0 1 -2 0'.split()))
-# items = tuple(map(int, input().split()))
+# items = tuple(map(int, '3 4 -5 2 6 1 -2 0'.split()))
+items = tuple(map(int, input().split()))
 
 bases, exps = items[::2], items[1::2]
-deri = []
+deri, zero = [], True
 
 for i in range(len(bases)):
-    base, exp = bases[i] * exps[i], exps[i] - 1
-    if base:
-        deri += [ base, exp ]
-    elif base == 0 and exp == 0:
-        deri += [0, 0]
+    if exps[i]:
+        deri += [ bases[i] * exps[i], exps[i] - 1 ]
+        zero = False
 
-# print(deri)
-print(' '.join(map(str, deri)))
+if zero:
+    print('0 0')
+else:
+    print(' '.join(map(str, deri)))
