498eb1b62012da6afa3811f5f26877ca9e30c8ee
zhangpeng96 <sjiaomail@gmail.com>
2020-05-26 13:19:14 +0800

    b1068, p3通过，题意中像素点独一无二是指在输入的像素图中有且仅有一个，p5非零返回

--- PAT-B/b1068/python-1068.py
+++ PAT-B/b1068/python-1068.py
@@ -1,11 +1,10 @@
 '''
     @name      : b1068
-    @version   : 20.0526
+    @version   : 20.0526.2
     @author    : zhangpeng96
-    @test_time : 102'50"
-    @pass_rate : p3, p5 failed, p4 timeout
+    @test_time : 106'20"
+    @pass_rate : p5 failed, p4 timeout
 '''
-from collections import Counter
 
 def check_bit(bit, checks, gap):
     return all( map(lambda n: abs(bit - n) > gap, checks) )
@@ -33,22 +32,21 @@ def search_bit(bitmap, x, y, right, bottom, gap):
     return check_bit(bitmap[x][y], checks, gap)
 
 
-def single_element(iters):
+def single_element(iters, bitmap_line):
     single = []
-    for value, times in Counter(iters).most_common()[::-1]:
-        if times == 1:
-            single += [value]
-        else:
-            break
+    for it in iters:
+        if bitmap_line.count(it) == 1:
+            single.append(it)
     return single
 
 
 width, height, gap = tuple(map(int, input().split()))
-bitmap = tuple([ tuple( map(int, input().split()) ) for _ in range(height) ])
+bitmap = [ list( map(int, input().split()) ) for _ in range(height) ]
 
 # width, height, gap = map(int, '8 6 200'.split())
 # inputs = ['0      0    0        0        0        0        0        0','65280    65280    65280    16711479 65280    65280    65280    65280','16711479 65280    65280    65280    16711680 65280    65280    65280','65280    65280    65280    65280    65280    65280    165280   165280','65280    65280    16777015 65280    65280    165280   65480    165280','16777215 16777215 16777215 16777215 16777215 16777215 16777215 16777215']
-# bitmap = tuple([ tuple( map(int, i.split()) ) for i in inputs ])
+# bitmap = [ list( map(int, i.split()) ) for i in inputs ]
+bitmap_line = sum(bitmap, [])
 
 right, bottom = width-1, height-1
 unique, option = {}, []
@@ -60,7 +58,7 @@ for x in range(height):
             option.append(bits)
             unique.update({ bits: (y+1,x+1) })
 
-option = single_element(option)
+option = single_element(option, bitmap_line)
 
 if len(option) == 1:
     print('({}, {}): {}'.format(*unique[option[0]], option[0]))
