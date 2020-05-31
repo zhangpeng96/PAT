ed5044bef467abac09827e712dc609a01f6c154a
zhangpeng96 <sjiaomail@gmail.com>
2020-05-31 21:45:00 +0800

    a1069，全部通过，未考虑输入数字格式无前导零

--- PAT-A/a1069/python-a1069.py
+++ PAT-A/a1069/python-a1069.py
@@ -1,19 +1,18 @@
 '''
     @name      : a1069
-    @version   : 20.0531
+    @version   : 20.0531.2
     @author    : zhangpeng96
-    @test_time : 12'22"
-    @pass_rate : p2, p3 failed, p4 timeout
+    @test_time : 13'50"
+    @pass_rate : all
 '''
 
 # sub = input()
-sub = '4'
+sub = int('99')
 
 while True:
-    sub = ''.join(sorted(sub))
+    sub = ''.join(sorted('{:04d}'.format(sub)))
     a, b = int(sub[::-1]), int(sub)
     sub = a - b
     print('{:04d} - {:04d} = {:04d}'.format(a, b, sub))
     if sub == 0 or sub == 6174:
         break
-    sub = str(sub)
