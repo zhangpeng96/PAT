4e5913681eac1a71655b1c5225cef634f60bebf1
zhangpeng96 <sjiaomail@gmail.com>
2020-06-01 20:21:33 +0800

    a1041, 传统list计数方法，p4,p5超时

---
+++ PAT-A/a1041-be-unique/python-a1041-2.py
@@ -0,0 +1,21 @@
+'''
+    @name      : a1041
+    @version   : 20.0601.2
+    @author    : zhangpeng96
+    @test_time : 
+    @pass_rate : p4, p5 timeout
+'''
+
+
+# ins = '7 5 31 5 88 67 88 17'
+# ins = '5 888 666 666 888 888'
+# digits = ins.split()[1:]
+digits = input().split()[1:]
+
+def search(digits):
+    for digit in digits:
+        if digits.count(digit) == 1:
+            return digit
+    return 'None'
+
+print(search(digits))
