f2a692caaec3547d9f17ed5ae2e3d150c0ba0be2
zhangpeng96 <sjiaomail@gmail.com>
2020-06-01 20:24:33 +0800

    a1041, 引入字典先判断是否为重复元素，p4,p5仍然超时

--- PAT-A/a1041-be-unique/python-a1041-2.py
+++ PAT-A/a1041-be-unique/python-a1041-2.py
@@ -1,6 +1,6 @@
 '''
     @name      : a1041
-    @version   : 20.0601.2
+    @version   : 20.0601.3
     @author    : zhangpeng96
     @test_time : 
     @pass_rate : p4, p5 timeout
@@ -11,11 +11,15 @@
 # ins = '5 888 666 666 888 888'
 # digits = ins.split()[1:]
 digits = input().split()[1:]
+repeat = set()
 
 def search(digits):
     for digit in digits:
-        if digits.count(digit) == 1:
-            return digit
+        if digit not in repeat:
+            if digits.count(digit) == 1:
+                return digit
+            else:
+                repeat.add(digit)
     return 'None'
 
 print(search(digits))
