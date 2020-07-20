eec928809200d346823e403dcece64c7e82eeed6
zhangpeng96 <sjiaomail@gmail.com>
2020-07-20 12:16:39 +0800

    a1060, p3,p5错误，错误原因是计算base时精度损失，如5 0.01234 0.012345

--- PAT-A/a1060-are-they-equal/python-a1060.py
+++ PAT-A/a1060-are-they-equal/python-a1060.py
@@ -1,25 +1,34 @@
 """
     @name      : a1060
-    @version   : 20.0719.2
+    @version   : 20.0720.1
     @author    : zhangpeng96
     @test_time : 32'00"
-    @pass_rate : p3, p5, p6 failed
+    @pass_rate : p3, p5 failed
 """
 
 # precision, digit_1, digit_2 = input().split()
 precision, digit_1, digit_2 = '3 120 128'.split()
-precision, digit_1, digit_2 = '1 0.0 0.1'.split()
+precision, digit_1, digit_2 = '5 0.01234 0.012345'.split()
 precision, digit_1, digit_2 = int(precision), float(digit_1), float(digit_2)
 
 def exp(digit):
     dig = int(digit)
+    a, b = str(digit).split('.')
     if dig:
-        return len(str(dig))
+        return len(a)-1
+    elif digit == 0.0:
+        return -1
     else:
-        return int('{:e}'.format(digit).split('e')[1]) + 1
+        count = -1
+        for i in b:
+            if i != '0':
+                break
+            count -= 1
+        return count
+
 
 def chopping(digit):
-    ep = exp(digit)
+    ep = exp(digit) + 1
     base = digit / (10**ep)
     r = 10**precision
     formats = '{:.' + str(precision) + 'f}*10^{}'
