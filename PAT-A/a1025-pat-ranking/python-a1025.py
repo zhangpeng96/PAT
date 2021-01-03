3ee24b3d9760373cd7e702ba83578c9bee76a579
zhangpeng96 <sjiaomail@gmail.com>
2021-01-03 16:32:54 +0800

    a1025全部通过

--- PAT-A/a1025-pat-ranking/python-a1025.py
+++ PAT-A/a1025-pat-ranking/python-a1025.py
@@ -3,14 +3,18 @@
     @version   : 21.0103
     @author    : zhangpeng96
     @test_time : 60'
-    @pass_rate : p3 failed
+    @pass_rate : all
 '''
 
 scores = []
 location_count = int(input())
 for i in range(location_count):
     test_count = int(input())
-    scores.append( [list(map(int, input().split())) for _ in range(test_count)] )
+    score = []
+    for _ in range(test_count):
+        text = input().split()
+        score.append([ text[0], int(text[1])])
+    scores.append(score)
 
 # scores = [[[1234567890001, 95], [1234567890005, 100], [1234567890003, 95], [1234567890002, 77], [1234567890004, 85]], [[1234567890013, 65], [1234567890011, 25], [1234567890014, 100], [1234567890012, 85]]]
 
@@ -26,6 +30,7 @@ def rank_with_same_value(values):
 
 
 for i, score in enumerate(scores):
+    # print(score)
     score.sort(key=lambda x:(-x[1], x[0]))
     rank = rank_with_same_value(list(map(lambda x:x[1], score)))
     scores_2.extend( list(map(lambda a,b: a + [ i+1, b], score, rank)) )
