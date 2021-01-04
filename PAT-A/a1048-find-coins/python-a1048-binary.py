9e7f723c67eb0d5ca5526f4f0321e39da35abca5
zhangpeng96 <sjiaomail@gmail.com>
2021-01-04 17:48:34 +0800

    a1048,采用二分查找法，部分测试点仍超时

---
+++ PAT-A/a1048-find-coins/python-a1048-binary.py
@@ -0,0 +1,41 @@
+'''
+    @name      : a1048
+    @version   : 21.0104
+    @author    : zhangpeng96
+    @test_time : 12'00"
+    @pass_rate : p1 failed, p3,p4 timeout
+'''
+
+# _, pay = map(int, input().split())
+# coins = list(map(int, input().split()))
+
+_, pay = map(int, '8 15'.split())
+coins = list(map(int, '1 2 8 7 2 4 11 15'.split()))
+
+coins.sort()
+ans = []
+
+def binary_search(coins, b):
+    low = 1
+    high = len(coins) - 1
+    while low <= high:
+        mid = int((low + high) / 2)
+        if coins[mid] == b:
+            return b
+        elif coins[mid] > b:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+for coin in coins:
+    b = pay - coin
+    search = binary_search(coins, b)
+    if search != -1:
+        ans = [coin, search]
+        break
+
+if ans:
+    print(ans[0], ans[1])
+else:
+    print('No Solution')
