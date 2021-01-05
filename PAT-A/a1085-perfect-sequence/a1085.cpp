4977e710eb9a171e130659d14f305be5ff07e3f2
zhangpeng96 <sjiaomail@gmail.com>
2021-01-05 18:22:00 +0800

    a1085，完美数列问题，不用upper_bound，自己写二分查找函数，p4超时，p5错误

---
+++ PAT-A/a1085-perfect-sequence/a1085.cpp
@@ -0,0 +1,39 @@
+/*
+    @name      : a1085
+    @version   : 21.0105
+    @author    : zhangpeng96
+    @pass_rate : p4 timeout, p5 failed
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int binary_search_upper(vector<int> seq, int key, int low, int high) {
+    while (low < high) {
+        int mid = (low + high) / 2;
+        if (seq[mid] > key)
+            high = mid;
+        else
+            low = mid + 1;
+    }
+    return low;
+}
+
+int main() {
+    int n, p, ans = 0;
+    cin >> n >> p;
+    vector<int> seq(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> seq[i];
+
+    sort(seq.begin(), seq.end());
+    for (int i = 0; i < n; i++) {
+        int low = binary_search_upper(seq, (long long) seq[i] * p, i, seq.size());
+        ans = max(ans, low - i);
+    }
+    cout << ans;
+    return 0;
+}
