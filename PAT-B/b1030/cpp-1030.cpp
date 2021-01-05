764615d8ddcdca4538c25c104db8ead91212a001
zhangpeng96 <sjiaomail@gmail.com>
2021-01-05 17:15:28 +0800

    b1030，C++使用标准库通过，p5错误

---
+++ PAT-B/b1030/cpp-1030.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, p, ans = 0;
+    cin >> n >> p;
+    vector<int> seq(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> seq[i];
+
+    sort(seq.begin(), seq.end());
+    for (int i = 0; i < n; i++) {
+        int low = upper_bound(seq.begin() + 1, seq.end(), seq[i] * p) - seq.begin();
+        ans = max(ans, low - i);
+    }
+    cout << ans;
+    return 0;
+}
