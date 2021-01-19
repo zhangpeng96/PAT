6d5b2f05de4fada3a0ed9ca5698f1acb0c74c2b1
zhangpeng96 <sjiaomail@gmail.com>
2021-01-19 14:02:35 +0800

    p1还要考虑输出无人选择的课程，无人选择的课程不输出名单

--- PAT-A/a1047-student-list-for-course/python-a1047.py
+++ PAT-A/a1047-student-list-for-course/python-a1047.py
@@ -3,21 +3,18 @@
     @version  : 21.0119
     @author   : zhangpeng96
     @time     : 26'00"
-    @accepted : p1 error
+    @accepted : all
 """
 
 n, k = map(int, input().split())
-data = {}
+data = dict(zip( [i+1 for i in range(k)], [ [] for _ in range(k) ] ))
 
 for _ in range(n):
     ins = input().split()
     for i in ins[2:]:
-        i = int(i)
-        if i in data:
-            data[i].append(ins[0])
-        else:
-            data[i] = [ins[0]]
+        data[int(i)].append(ins[0])
 
 for item in sorted(data.items(), key=lambda x:x[0]):
     print(item[0], len(item[1]))
-    print('\n'.join(sorted(item[1])))
+    if len(item[1]):
+        print('\n'.join(sorted(item[1])))
