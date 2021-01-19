f4906aafbc13be5ba16890a7aab46f1442a64da3
zhangpeng96 <sjiaomail@gmail.com>
2021-01-19 18:16:36 +0800

    a1096，思路错误

--- PAT-A/a1096-consecutive-factors/python-a1096.py
+++ PAT-A/a1096-consecutive-factors/python-a1096.py
@@ -1,12 +1,26 @@
 from math import sqrt
 
-number = int('360')
+# number = int('630')
+number = int(input())
+max_n = int(sqrt(number)) + 1
 factors = []
+result = []
 
-for n in range(1, int(sqrt(number))):
-    product = n
-    while product <= number:
-        n += 1
-        product *= n
-        if product == number:
-            print(n) 
+for a in range(2, max_n):
+    product = 1
+    for b in range(a, max_n):
+        product *= b
+        print(a, b, product)
+        if number % product:
+            break
+        else:
+            length = b-a+1
+            if product*length == number:
+                result.append((length, a, b))
+
+if len(result):
+    length, start, end = sorted(result, key=lambda x: (-x[0], x[1]))[0]
+    print(length)
+    print('*'.join(map(str, [i for i in range(start, end+1) ])))
+else:
+    print('1\n{}'.format(number))
