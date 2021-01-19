ab538bd1f51bf43ebb65a5d270a892c0528a1f57
zhangpeng96 <sjiaomail@gmail.com>
2021-01-19 18:22:25 +0800

    a1096，全部通过

--- PAT-A/a1096-consecutive-factors/python-a1096.py
+++ PAT-A/a1096-consecutive-factors/python-a1096.py
@@ -1,22 +1,26 @@
+"""
+    @name     : a1096
+    @version  : 21.0119
+    @author   : zhangpeng96
+    @time     : >60'00"
+    @accepted : all
+"""
+
 from math import sqrt
 
-# number = int('630')
+# number = int('14')
 number = int(input())
 max_n = int(sqrt(number)) + 1
-factors = []
 result = []
 
-for a in range(2, max_n):
+for a in range(2, max_n+1):
     product = 1
-    for b in range(a, max_n):
+    for b in range(a, max_n+1):
         product *= b
-        print(a, b, product)
         if number % product:
             break
         else:
-            length = b-a+1
-            if product*length == number:
-                result.append((length, a, b))
+            result.append((b-a+1, a, b))
 
 if len(result):
     length, start, end = sorted(result, key=lambda x: (-x[0], x[1]))[0]
