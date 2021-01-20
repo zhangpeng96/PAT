678e6290af836d8ba79cf9b9774b65cc17bd6841
zhangpeng96 <sjiaomail@gmail.com>
2021-01-20 08:43:00 +0800

    a1130，树节点不设默认值，均由输入给出，传入元组，避免列表多重下标影响可读性

--- PAT-A/a1130-infix-expression/python-a1130-recursion.py
+++ PAT-A/a1130-infix-expression/python-a1130-recursion.py
@@ -1,25 +1,26 @@
 """
     @name     : a1130
-    @version  : 21.0120（递归建树，中序遍历输出）
+    @version  : 21.0120.2（递归建树，中序遍历输出）
     @author   : zhangpeng96
     @time     : >60'00"
     @accepted : all
 """
 
 class BTNode:
-    def __init__(self, val):
+    def __init__(self, tup):
+        val, left, right = tup
         self.val = val
-        self.left = -1
-        self.right = -1
+        self.left = left
+        self.right = right
 
 class BT():
     def insert(self, root, node, index):
-        val, left, right = node[index][0], node[index][1], node[index][2]
+        left, right = node[index][1:]
         if left != -1:
-            root.left = BTNode(node[left][0])
+            root.left = BTNode(node[left])
             root.left = self.insert(root.left, node, left)
         if right != -1:
-            root.right = BTNode(node[right][0])
+            root.right = BTNode(node[right])
             root.right = self.insert(root.right, node, right)
         return root
 
@@ -41,23 +42,23 @@ def find_root(nodes):
 nodes = []
 result = []
 
-ins = ['* 8 7','a -1 -1','* 4 1','+ 2 5','b -1 -1','d -1 -1','- -1 6','c -1 -1']
+# ins = ['* 8 7','a -1 -1','* 4 1','+ 2 5','b -1 -1','d -1 -1','- -1 6','c -1 -1']
 # ins = ['2.35 -1 -1','* 6 1','- -1 4','% 7 8','+ 2 3','a -1 -1','str -1 -1','871 -1 -1']
-for node in ins:
-    val, left, right = node.split()
-    nodes.append((val, int(left), int(right)))
-
-# count = int(input())
-# for _ in range(count):
-#     val, left, right = input().split()
+# for node in ins:
+#     val, left, right = node.split()
 #     nodes.append((val, int(left), int(right)))
+
+count = int(input())
+for _ in range(count):
+    val, left, right = input().split()
+    nodes.append((val, int(left), int(right)))
 
 head = find_root(nodes)
 nodes.insert(0, head)
 
 bt = BT()
-tree = BTNode(nodes[head][0])
-tree.left, tree.right = nodes[head][1:]
+tree = BTNode(nodes[head])
+
 bt.insert(tree, nodes, head)
 bt.in_order(tree, result, 0)
 print(''.join(result))
