f1bb700b9a96fc996bb55e07cf19ff161406307c
zhangpeng96 <sjiaomail@gmail.com>
2021-01-22 21:51:18 +0800

    a1099，全部通过

--- PAT-A/a1099-build-a-binary-search-tree/python-a1099.py
+++ PAT-A/a1099-build-a-binary-search-tree/python-a1099.py
@@ -1,3 +1,10 @@
+"""
+    @name     : a1099
+    @version  : 21.0122
+    @author   : zhangpeng96
+    @time     : 35'40"
+    @accepted : all
+"""
 
 class BTNode:
     def __init__(self):
@@ -25,26 +32,23 @@ class BST():
                 queue.append(nodes[node.right])
 
 
-bt = BST()
-# tree = None
+# ptr = ['1 6','2 3','-1 -1','-1 4','5 -1','-1 -1','7 -1','-1 8','-1 -1']
+# seq = list(map(int, '73 45 11 58 82 25 67 38 42'.split()))
 
-# for val in seq:
-#     tree = bt.insert(tree, val)
-count = int('9')
+count = int(input())
 nodes = [ BTNode() for _ in range(count) ]
-ptr = ['1 6','2 3','-1 -1','-1 4','5 -1','-1 -1','7 -1','-1 8','-1 -1']
 
 for i in range(count):
-    left, right = map(int, ptr[i].split())
+    left, right = map(int, input().split())
     nodes[i].left = left
     nodes[i].right = right
 
-seq = list(map(int, '73 45 11 58 82 25 67 38 42'.split()))
+seq = list(map(int, input().split()))
 seq.sort()
-bt.in_order_insert(0, nodes, iter(seq))
-level_traversal = []
-bt.level_order(0, nodes, level_traversal)
-print(level_traversal)
-for n in nodes:
-    print(n.val)
 
+bt = BST()
+bt.in_order_insert(0, nodes, iter(seq))
+
+result = []
+bt.level_order(0, nodes, result)
+print(' '.join(map(str, result)))
