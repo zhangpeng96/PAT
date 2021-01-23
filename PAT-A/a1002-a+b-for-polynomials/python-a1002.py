c53265c5610f624f921c2aca5dc32791d2d60a14
zhangpeng96 <sjiaomail@gmail.com>
2021-01-23 22:12:11 +0800

    a1002，全部通过

--- PAT-A/a1002-a+b-for-polynomials/python-a1002.py
+++ PAT-A/a1002-a+b-for-polynomials/python-a1002.py
@@ -1,24 +1,31 @@
+"""
+    @name     : a1002
+    @version  : 21.0123
+    @author   : zhangpeng96
+    @time     : 12'00"
+    @accepted : all
+"""
 
-
-ins1 = '2 1 2.4 0 3.2'
-ins2 = '2 2 1.5 1 0.5'
+# ins1 = '2 1 2.4 0 3.2'
+# ins2 = '2 2 1.5 1 0.5'
 
 ins1 = input()
 ins2 = input()
 
-poly_a = dict(zip( map(int, ins1.split()[1::2]), map(float, ins1.split()[2::2]) ))
-poly_b = dict(zip( map(int, ins2.split()[1::2]), map(float, ins2.split()[2::2]) ))
+a = dict(zip( map(int, ins1.split()[1::2]), map(float, ins1.split()[2::2]) ))
+b = dict(zip( map(int, ins2.split()[1::2]), map(float, ins2.split()[2::2]) ))
 poly = {}
 
-for key in (poly_a.keys() | poly_b.keys()):
-    poly[key] = poly_a.get(key, 0) + poly_b.get(key, 0)
+for key in (a.keys() | b.keys()):
+    total = a.get(key, 0.0) + b.get(key, 0.0)
+    if total != 0.0:
+        poly[key] = total
 
-res = sorted(poly.items(), key = lambda x: -x[0])
+poly = sorted(poly.items(), reverse=True)
+line = ' '.join(map(lambda x: '{:d} {:.1f}'.format(*x), poly))
+k = len(poly)
 
-print(len(res), ' '.join(map(lambda x: '{} {}'.format(*x), res)))
-
-
-# from collections import Counter
-# a, b = Counter(poly_a), Counter(poly_b)
-
-# print(a, b, a+b)
+if k:
+    print(k, line)
+else:
+    print(0)
