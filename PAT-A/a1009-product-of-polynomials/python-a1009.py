00a0548af4266cc27b5957d30bfcf6fd25ed196f
zhangpeng96 <sjiaomail@gmail.com>
2021-01-24 10:12:23 +0800

    a1009，全部通过

--- PAT-A/a1009-product-of-polynomials/python-a1009.py
+++ PAT-A/a1009-product-of-polynomials/python-a1009.py
@@ -1,34 +1,33 @@
+"""
+    @name     : a1009
+    @version  : 21.0123
+    @author   : zhangpeng96
+    @time     : 32'28"
+    @accepted : all
+"""
 
-# input_a = input()
-# input_b = input()
+from itertools import product
 
-input_a = '3 1 2.4 0 3.2 5 0.0'
-input_b = '2 2 1.5 1 0.5'
+# ins1 = '2 1 2.4 0 3.2'
+# ins2 = '2 2 1.5 1 0.5'
 
+ins1 = input()
+ins2 = input()
 
-a_count, b_count = int(input_a[0]), int(input_b[0])
-poly_a_coe, poly_a_exp = tuple(map(float, input_a.split()[2::2])), tuple(map(int, input_a.split()[1::2]))
-poly_b_coe, poly_b_exp = tuple(map(float, input_b.split()[2::2])), tuple(map(int, input_b.split()[1::2]))
-
+a = zip( map(int, ins1.split()[1::2]), map(float, ins1.split()[2::2]) )
+b = zip( map(int, ins2.split()[1::2]), map(float, ins2.split()[2::2]) )
 poly = {}
 
-for a in range(a_count):
-    for b in range(b_count):
-        coe, exp = poly_a_coe[a] * poly_b_coe[b], poly_a_exp[a] + poly_b_exp[b]
-        if exp in poly.keys():
-            poly[exp] += coe
-        else:
-            poly[exp] = coe
+prod = map(lambda x: (x[0][0]+x[1][0], x[0][1]*x[1][1]), product(a, b))
 
-poly = sorted(filter(lambda x: x[1] != 0.0, poly.items()), key = lambda x: -x[0])
+for exp, coef in prod:
+    poly[exp] = poly.get(exp, 0.0) + coef
 
-print(poly)
+poly = list( filter(lambda x:x[1]!=0.0, sorted(poly.items(),reverse=True)) )
+line = ' '.join(map(lambda x: '{:d} {:.1f}'.format(*x), poly))
+k = len(poly)
 
-print(len(poly), ' '.join( map(lambda x: '{:d} {:.1f}'.format(*x), poly) ))
-
-# from itertools import product
-
-# co = product(poly_a_coe, poly_b_coe)
-# ep = product(poly_a_exp, poly_b_exp)
-# print(list(co), list(ep))
-
+if k:
+    print(k, line)
+else:
+    print(0)
