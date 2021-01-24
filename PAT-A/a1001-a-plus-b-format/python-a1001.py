33fe9c0a2ed6d5b503791da544e9b7ff0ab5531a
zhangpeng96 <sjiaomail@gmail.com>
2021-01-24 11:25:39 +0800

    a1001, 通过

---
+++ PAT-A/a1001-a-plus-b-format/python-a1001.py
@@ -0,0 +1,10 @@
+"""
+    @name     : a1001
+    @version  : 21.0124
+    @author   : zhangpeng96
+    @time     : 0'40"
+    @accepted : all
+"""
+
+a, b = map(int, input().split())
+print('{:,d}'.format(a + b))
