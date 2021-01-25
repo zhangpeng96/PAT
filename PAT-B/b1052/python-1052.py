265d948199e4eb0af253e71a8e64409e49fde049
zhangpeng96 <sjiaomail@gmail.com>
2021-01-25 22:09:42 +0800

    b1052，测试点1输入序号为0，测试点2输入序号为负数，修改后全部通过

--- PAT-B/b1052/python-1052.py
+++ PAT-B/b1052/python-1052.py
@@ -3,7 +3,7 @@
     @version  : 21.0125
     @author   : zhangpeng96
     @time     : 50'00"
-    @accepted : p1,p2 error
+    @accepted : all
 """
 
 import sys
@@ -22,8 +22,8 @@ eyes = r.findall(eyes)
 mouths = r.findall(mouths)
 
 for p1, p2, p3, p4, p5 in parts:
-    try:
+    if 0<p1<=len(hands) and 0<p2<=len(eyes) and 0<p3<=len(mouths) and 0<p4<=len(eyes) and 0<p5<=len(hands):
         line = hands[p1-1] + b'(' + eyes[p2-1] + mouths[p3-1] + eyes[p4-1] + b')' + hands[p5-1] + b'\n'
         sys.stdout.buffer.write(line)
-    except:
+    else:
         sys.stdout.buffer.write(b'Are you kidding me? @\/@\n')
