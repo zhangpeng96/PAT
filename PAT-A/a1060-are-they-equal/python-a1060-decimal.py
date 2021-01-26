149f0106f6e7c839eb3d6e183e2a81ffedaf11a6
zhangpeng96 <sjiaomail@gmail.com>
2021-01-26 19:36:47 +0800

    a1060，全部通过，p6需要考虑0的特殊情况，无论多少位有效数字的0，都是0，0转换为任意形式的科学计数法时都不做小数点的调整

--- PAT-A/a1060-are-they-equal/python-a1060-decimal.py
+++ PAT-A/a1060-are-they-equal/python-a1060-decimal.py
@@ -3,18 +3,18 @@
     @version  : 21.0126
     @author   : zhangpeng96
     @time     : 54'00"
-    @accepted : p6 error
+    @accepted : all
 """
 
 from decimal import Decimal, getcontext, ROUND_DOWN
 
-prec, a, b = '3 120 128'.split()
+# prec, a, b = '3 120 128'.split()
 # prec, a, b = '1 2 1'.split()
 # prec, a, b = '5 00.01 0.0001'.split()
 # prec, a, b = '4 0.01234 0.012345'.split()
 # prec, a, b = '5 0.1234 0.12345'.split()
 # prec, a, b = '1 12300 12358.95'.split()
-# prec, a, b = '1 00.0 0.000'.split()
+prec, a, b = '1 00.0 0.000'.split()
 
 prec, a, b = input().split()
 prec, a, b = int(prec), Decimal(a), Decimal(b)
@@ -22,7 +22,10 @@ prec, a, b = int(prec), Decimal(a), Decimal(b)
 def stand(a, prec):
     getcontext().prec = prec
     getcontext().rounding = ROUND_DOWN
-    pos = 1 + a.adjusted()
+    if a == Decimal(0):
+        pos = 0
+    else:
+        pos = 1 + a.adjusted()
     power = Decimal(10) ** Decimal(-pos)
     s = a * power
     return s, pos
