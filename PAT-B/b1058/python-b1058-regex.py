53332c540c45779d079645a9635b37813f34e8cb
zhangpeng96 <sjiaomail@gmail.com>
2021-01-27 14:57:37 +0800

    步骤上再优化

--- PAT-B/b1058/python-b1058-regex.py
+++ PAT-B/b1058/python-b1058-regex.py
@@ -1,33 +1,36 @@
+"""
+    @name     : b1058
+    @version  : 21.0127
+    @author   : zhangpeng96
+    @time     : 43'00"
+    @accepted : all
+"""
+
 import re
 
 n, m = map(int, input().split())
 
 keys, scores = [], []
-students, totals, wrongs = [], [0]*n, {}
+wrongs = {}
 regex = r'(?<=\(\d\s)(.*?)(?=\))'
 
 for _ in range(m):
     score, _, _, *key = input().split()
     scores.append(int(score))
-    keys.append(set(key))
+    keys.append(key)
 
-for _ in range(n):
-    answer = [ set(r.split()) for r in re.findall(regex, input()) ]
-    students.append(answer)
-
-for i, student in enumerate(students):
-    for j, answer in enumerate(student):
-        if answer == keys[j]:
-            totals[i] += scores[j]
+for i in range(n):
+    total = 0
+    for j, r in enumerate(re.findall(regex, input())):
+        if r.split() == keys[j]:
+            total += scores[j]
         else:
             wrongs[j+1] = wrongs.get(j+1, 0) + 1
-
-print('\n'.join(map(str, totals)))
+    print(total)
 
 if wrongs:
-    result = sorted(wrongs.items(), key=lambda x:-x[1])
-    count = result[0][1]
-    items = sorted(map(lambda x:x[0], filter(lambda x: x[1]==count, result)))
+    count = max(wrongs.values())
+    items = sorted(map(lambda x:x[0], filter(lambda x: x[1]==count, wrongs.items())))
     print(count, ' '.join(map(str, items)))
 else:
     print('Too simple')
