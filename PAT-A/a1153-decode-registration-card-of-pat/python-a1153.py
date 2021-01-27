5c4b6276e45cf59a037ae39f5d3615c3e84d6d83
zhangpeng96 <sjiaomail@gmail.com>
2021-01-27 23:05:25 +0800

    a1153，仍有两个测试点超时，无法优化

--- PAT-A/a1153-decode-registration-card-of-pat/python-a1153.py
+++ PAT-A/a1153-decode-registration-card-of-pat/python-a1153.py
@@ -1,4 +1,5 @@
 from itertools import groupby
+
 def op1(op):
     p = sorted(filter(lambda r:r[2]==op, record), key=lambda x:(-x[1], x[0]))
     return '\n'.join( map(lambda x: '{} {}'.format(x[0], x[1]), p) )
@@ -16,17 +17,17 @@ def op3(op):
     q = sorted( q, key=lambda x:(-x[1], x[0]) )
     return '\n'.join( map(lambda x: '{} {}'.format(*x), q) )
 
-
-data_list = ['B123180908127 99', 'B102180908003 86', 'A112180318002 98', 'T107150310127 62', 'A107180908108 100', 'T123180908010 78', 'B112160918035 88', 'A107180908021 98']
-query_list = ['1 A','2 107','3 180908','2 999']
 record = []
 query_id = 0
 
-for data in data_list:
-    ids, score = data.split()
+n, m = map(int, input().split())
+
+for _ in range(n):
+    ids, score = input().split()
     record.append( (ids, int(score), ids[0], ids[1:4], ids[4:10], ids[10:13]) )
 
-for query in query_list:
+for _ in range(m):
+    query = input()
     query_id += 1
     print('Case {}: {}'.format(query_id, query))
     f, op = query.split()
