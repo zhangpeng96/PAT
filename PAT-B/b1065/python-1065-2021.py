3dcb1617928b48ad3aa62eaf3ad4c95a7762b30b
zhangpeng96 <sjiaomail@gmail.com>
2021-01-28 21:27:16 +0800

    a1065，将列表转换为集合后，查找元素是否在其中速度显著提高

--- PAT-B/b1065/python-1065-2021.py
+++ PAT-B/b1065/python-1065-2021.py
@@ -1,16 +1,22 @@
+"""
+    @name     : a1065
+    @version  : 21.0128
+    @author   : zhangpeng96
+    @time     : 19'15"
+    @accepted : all
+"""
 
+partner = {}
 
 def dog(v):
     w = partner.get(v, False)
     if w:
-        if w not in visitor: return True
+        if w not in visitor_pool: return True
     else:
         return True
     return False
 
 
-
-partner = {}
 n = int(input())
 for _ in range(n):
     cp1, cp2 = input().split()
@@ -19,6 +25,7 @@ for _ in range(n):
 
 m = int(input())
 visitor = input().split()
+visitor_pool = set(visitor)
 
 single = sorted(filter(dog, visitor))
 count = len(single)
