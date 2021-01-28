ce58f1b26cc87922bf787b3f2fd067b310cf4d70
zhangpeng96 <sjiaomail@gmail.com>
2021-01-28 16:54:30 +0800

    a1103，仍然一个点超时

--- PAT-A/a1103-integer-factorization/python-a1103.py
+++ PAT-A/a1103-integer-factorization/python-a1103.py
@@ -1,19 +1,30 @@
+"""
+    @name     : a1103
+    @version  : 21.0128
+    @author   : zhangpeng96
+    @time     : >60'00"
+    @accepted : p5 timeout
+"""
+
 from copy import copy
 
-number, count, power = map(int, '169 5 2'.split())
+number, count, power = map(int, input().split())
 
 result, powers, path = [], [], [None]*count
-temp, index = 0, 1
+temp, index, factor_sum = 0, 1, 0
 while temp <= number:
     powers.append(temp)
     temp = index**power
     index += 1
 
 def dfs(index, temp_number, temp_count):
+    global factor_sum, result
     if temp_count == count:
         if temp_number == number:
-            result.append( (sum(path), copy(path)) )
-            print(result)
+            factor = sum(path)
+            if factor > factor_sum:
+                result = copy(path)
+                factor_sum = factor
         return
     while index >= 1:
         if temp_number + powers[index] <= number:
@@ -22,17 +33,18 @@ def dfs(index, temp_number, temp_count):
         if index == 1:
             return
         index -= 1
-    # for i in range(index, 0, -1):
-    #     if temp_number + powers[i] <= number:
-    #         path[temp_count] = i
-    #         dfs(i, temp_number + powers[i], temp_count + 1)
-    #     if i == 1:
-    #         return
-
+"""也可以用for语句代替while语句
+    for i in range(index, 0, -1):
+        if temp_number + powers[i] <= number:
+            path[temp_count] = i
+            dfs(i, temp_number + powers[i], temp_count + 1)
+        if i == 1:
+            return
+"""
 dfs(len(powers)-1, 0, 0)
 
 if result:
-    bases = map(lambda x: '{}^{}'.format(x, power), sorted(result)[-1][1])
+    bases = map(lambda x: '{}^{}'.format(x, power), result)
     print('{} = {}'.format(number, ' + '.join(bases)))
 else:
     print('Impossible')
