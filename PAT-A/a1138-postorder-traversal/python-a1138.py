70f5eb57f271328f5f1c635520e794b596e161b0
zhangpeng96 <sjiaomail@gmail.com>
2021-02-04 12:40:45 +0800

    a1138，直接输出后序序列，仍然超时

--- PAT-A/a1138-postorder-traversal/python-a1138.py
+++ PAT-A/a1138-postorder-traversal/python-a1138.py
@@ -6,28 +6,14 @@
     @accepted : p4,p5 timeout
 """
 
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-
 def create(root, start, end):
-    if start > end: return None
+    if start > end: return
     i = start
     while i < end and in_order[i] != pre_order[root]:
         i += 1
-    node = Node(pre_order[root])
-    node.left = create(root+1, start, i-1)
-    node.right = create(root+1+(i-start), i+1, end)
-    return node
-
-def post_order(root, lst):
-    if root == None: return
-    post_order(root.left, lst)
-    post_order(root.right, lst)
-    lst.append(root.val)
-
+    create(root+1, start, i-1)
+    create(root+1+(i-start), i+1, end)
+    post_traversal.append(pre_order[root])
 
 
 count = int(input())
@@ -35,8 +21,6 @@ pre_order = list(map(int, input().split()))
 in_order = list(map(int, input().split()))
 
 post_traversal = []
-bt = create(0, 0, count-1)
-post_order(bt, post_traversal)
-
+create(0, 0, count-1)
 print(post_traversal[0])
-# print(' '.join(map(str, post_traversal)))
+
