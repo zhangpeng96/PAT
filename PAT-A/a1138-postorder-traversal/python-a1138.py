2a2d09ab497407a99aa7c01a9d7f6ccb965a9bda
zhangpeng96 <sjiaomail@gmail.com>
2021-02-04 12:46:37 +0800

    a1138，设置全局遍历提前结束递归，p4仍超时，有利于深入理解递归

--- PAT-A/a1138-postorder-traversal/python-a1138.py
+++ PAT-A/a1138-postorder-traversal/python-a1138.py
@@ -3,10 +3,12 @@
     @version  : 21.0203
     @author   : zhangpeng96
     @time     : 15'00"
-    @accepted : p4,p5 timeout
+    @accepted : p4 timeout
 """
 
 def create(root, start, end):
+    global finish
+    if finish: return
     if start > end: return
     i = start
     while i < end and in_order[i] != pre_order[root]:
@@ -14,13 +16,14 @@ def create(root, start, end):
     create(root+1, start, i-1)
     create(root+1+(i-start), i+1, end)
     post_traversal.append(pre_order[root])
+    finish = True
 
 
 count = int(input())
 pre_order = list(map(int, input().split()))
 in_order = list(map(int, input().split()))
 
+finish = False
 post_traversal = []
 create(0, 0, count-1)
 print(post_traversal[0])
-
