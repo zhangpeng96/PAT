0b65156c1f76cb03c58025830cebd2533ac34079
zhangpeng96 <sjiaomail@gmail.com>
2021-02-06 18:19:47 +0800

    a1134，优化后可全部通过

--- PAT-A/a1134-vertex-cover/python-a1134.py
+++ PAT-A/a1134-vertex-cover/python-a1134.py
@@ -3,23 +3,24 @@
     @version  : 21.0206
     @author   : zhangpeng96
     @time     : 54'12"
-    @accepted : p2 timeout
+    @accepted : all
 """
 
+from copy import copy
+
 vert_in_edge = {}
-edge_cover = {}
 
 vert_count, edge_count = map(int, input().split())
+edge_covers = dict(zip( [i for i in range(edge_count)], [False]*edge_count ))
 
 for i in range(edge_count):
-    v1, v2 = map(int, input().split())
+    v1, v2 = input().split()
     vert_in_edge[v1] = vert_in_edge.get(v1, []) + [i]
     vert_in_edge[v2] = vert_in_edge.get(v2, []) + [i]
 
 for _ in range(int(input())):
-    nodes = map(int, input().split()[1:])
-    edge_cover = dict(zip( [i for i in range(edge_count)], [False]*edge_count ))
-
+    nodes = input().split()[1:]
+    edge_cover = copy(edge_covers)
     for node in nodes:
         for edge in vert_in_edge.get(node, []):
             edge_cover[edge] = True
