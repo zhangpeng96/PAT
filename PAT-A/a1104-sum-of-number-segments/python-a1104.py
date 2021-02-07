1229cdbf24a2eba31ed087fbc54469c2d2ba70ad
zhangpeng96 <sjiaomail@gmail.com>
2021-02-07 13:08:31 +0800

    a1104，b1049，数学递推方法，全部通过

---
+++ PAT-A/a1104-sum-of-number-segments/python-a1104.py
@@ -0,0 +1,21 @@
+"""
+    @name     : a1049
+    @version  : 21.0207
+    @author   : zhangpeng96
+    @time     : 27'00"
+    @accepted : all
+    @desc     : 必须用decimal，否则p2精度不够出错
+    https://www.dazhuanlan.com/2019/11/30/5de16b44d92f3/
+"""
+
+from decimal import Decimal
+
+count = int(input())
+digit = map(Decimal, input().split())
+
+result = sum(map(
+    lambda i, x: x*(count - i)*(i + 1),
+    range(count), digit
+))
+
+print('{:.2f}'.format(result))
