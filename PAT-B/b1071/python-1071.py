d930d77d755498963b2cc72a66aa244277b73c3c
zhangpeng96 <sjiaomail@gmail.com>
2021-02-07 12:33:21 +0800

    b1071，全部通过

--- PAT-B/b1071/python-1071.py
+++ PAT-B/b1071/python-1071.py
@@ -1,12 +1,13 @@
+"""
+    @name     : b1071
+    @version  : 21.0207
+    @author   : zhangpeng96
+    @time     : 12'00"
+    @accepted : all
+"""
 
-# token_bag, count = tuple(map(int, '100 4'.split()))
-# rounds = ['8 0 100 2','3 1 50 1','5 1 200 6','7 0 200 8']
-
-token_bag, count = tuple(map(int, '100 4'.split()))
-rounds = ['8 0 100 2','3 1 200 1','5 1 200 6','7 0 200 8']
-
-def battle(round_str, token_bag):
-    n1, bigger, token, n2 = tuple(map(int, round_str.split()))
+def battle(token_bag):
+    n1, bigger, token, n2 = map(int, input().split())
     if token > token_bag:
         print('Not enough tokens.  Total = {}.'.format(token_bag))
         return token_bag
@@ -24,11 +25,10 @@ def battle(round_str, token_bag):
     return token_bag
 
 
-# token_bag, count = tuple(map(int, input().split()))
-# rounds = [input() for _ in range(count)]
+token_bag, count = tuple(map(int, input().split()))
 
-for r in rounds:
-    token_bag = battle(r, token_bag)
+for _ in range(count):
+    token_bag = battle(token_bag)
     if token_bag <= 0:
         print('Game Over.')
         break
