c7e1298f363abcc9a2722916abbf62a100ebad37
zhangpeng96 <sjiaomail@gmail.com>
2021-02-08 20:49:00 +0800

    b1075,p2非零返回，p5超时

---
+++ PAT-B/b1075/python-b1075.py
@@ -0,0 +1,39 @@
+# from sys import stdin
+
+head, count, k = input().split()
+count, k = int(count), int(k)
+
+# lines = stdin.readlines()
+# link_list = dict( zip(map(lambda x:x.split()[0], lines), map(lambda x:x.split()[1:], lines)) )
+link_list = {}
+for _ in range(count):
+    address, data, succr = input().split()
+    link_list[address] = [int(data), succr, 9999999]
+
+order = 0
+succr = head
+while succr != '-1':
+    if link_list[succr][0] < 0:
+        link_list[succr][2] = order
+        order += 1
+    succr = link_list[succr][1]
+
+succr = head
+while succr != '-1':
+    if 0 <= link_list[succr][0] <= k:
+        link_list[succr][2] = order
+        order += 1
+    succr = link_list[succr][1]
+
+succr = head
+while succr != '-1':
+    if link_list[succr][0] > k:
+        link_list[succr][2] = order
+        order += 1
+    succr = link_list[succr][1]
+
+link_list = sorted(link_list.items(), key=lambda x:x[1][2])
+
+for i in range(order-1):
+    print('{} {} {}'.format(link_list[i][0], link_list[i][1][0], link_list[i+1][0]))
+print('{} {} -1'.format(link_list[i+1][0], link_list[i+1][1][0]))
