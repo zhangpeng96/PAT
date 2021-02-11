e401d0ec8192e4eadbe7660d0da2e7674e4465b0
zhangpeng96 <sjiaomail@gmail.com>
2021-02-11 17:39:08 +0800

    7-3读题错误，修正

--- PAT-Exam/PAT-Basic-2019-Autumn/room/7-3.py
+++ PAT-Exam/PAT-Basic-2019-Autumn/room/7-3.py
@@ -3,17 +3,17 @@
     @version  : 21.0119
     @author   : zhangpeng96
     @time     : 41'07"
-    @accepted : p0 timeout
-                p1 timeout
+    @accepted : p0 error
+                p1 right +1
                 p2 timeout
                 p3 error
                 p4 timeout
 """
 
-from itertools import permutations
+# from math import sqrt
 
 def judge(a, b):
-    if a**3 - (a-1)**3 == (b**2 + (b-1)**2)**2:
+    if ( 3*a*a - 3*a + 1) == ( (2*b*b - 2*b + 1) * (2*b*b - 2*b + 1) ):
         return True
     else:
         return False
@@ -21,10 +21,11 @@ def judge(a, b):
 start, end = map(int, input().split())
 solution = False
 
-for a, b in permutations(range(start, end+1), 2):
-    if judge(a, b):
-        print(a, b)
-        solution = True
+for a in range(start, end+1):
+    for b in range( int(pow( ( 3*a*a - 3*a + 1)//4, 0.25)) ):
+        if judge(a, b):
+            print(a, b)
+            solution = True
 
 if not solution:
     print('No Solution')
