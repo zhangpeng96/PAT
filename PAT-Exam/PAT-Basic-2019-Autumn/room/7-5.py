7421671b5acb4a907c44e88ebb1d2f15666c9de2
zhangpeng96 <sjiaomail@gmail.com>
2021-02-11 18:11:51 +0800

    p3要考虑长链比短链长2倍以上并且存在无效节点

--- PAT-Exam/PAT-Basic-2019-Autumn/room/7-5.py
+++ PAT-Exam/PAT-Basic-2019-Autumn/room/7-5.py
@@ -6,7 +6,7 @@
     @accepted : p0 right +12
                 p1 right +3
                 p2 right +1
-                p3 error
+                p3 right +3
                 p4 timeout
 """
 
@@ -14,9 +14,7 @@ class Node:
     def __init__(self, data, succr):
         self.data = data
         self.next = succr
-        self.order = 0
-    def __repr__(self):
-        return '({})[head | {} | {} ]'.format(self.order, self.data, self.next)
+        self.order = 9999999
 
 def length(link, head):
     count = 0
@@ -48,17 +46,18 @@ if length1 < length2:
     head1, head2 = head2, head1
     length1, length2 = length2, length1
 
+count = length1 + length2
 head2 = reverse(link, head2)
 
 for i in range(1, count+1):
-    if i % 3 and head1 != '-1':
+    if ((i % 3 != 0) and head1 != '-1') or (head2 == '-1' and head1 != '-1'):
         link[head1].order = i
         head1 = link[head1].next
-    if not i % 3 and head2 != '-1':
+    if (i % 3 == 0) and head2 != '-1':
         link[head2].order = i
         head2 = link[head2].next
-
+        
 link = sorted(link.items(), key=lambda x:x[1].order)
-for i in range(len(link)-1):
+for i in range(count-1):
     print('{} {} {}'.format(link[i][0], link[i][1].data, link[i+1][0]))
 print('{} {} -1'.format(link[count-1][0], link[count-1][1].data))
