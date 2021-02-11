913c49d3c218354bebfe740c0a1e839cb86a44e7
zhangpeng96 <sjiaomail@gmail.com>
2021-02-11 17:42:12 +0800

    根据a的大小来判断b的范围上界，开根号向上取整

--- PAT-Exam/PAT-Basic-2019-Autumn/room/7-3.py
+++ PAT-Exam/PAT-Basic-2019-Autumn/room/7-3.py
@@ -3,14 +3,14 @@
     @version  : 21.0119
     @author   : zhangpeng96
     @time     : 41'07"
-    @accepted : p0 error
+    @accepted : p0 right +12
                 p1 right +1
                 p2 timeout
-                p3 error
+                p3 right +2
                 p4 timeout
 """
 
-# from math import sqrt
+from math import ceil
 
 def judge(a, b):
     if ( 3*a*a - 3*a + 1) == ( (2*b*b - 2*b + 1) * (2*b*b - 2*b + 1) ):
@@ -22,7 +22,7 @@ start, end = map(int, input().split())
 solution = False
 
 for a in range(start, end+1):
-    for b in range( int(pow( ( 3*a*a - 3*a + 1)//4, 0.25)) ):
+    for b in range( ceil(pow( ( 3*a*a - 3*a + 1)//4, 0.25))+1 ):
         if judge(a, b):
             print(a, b)
             solution = True
