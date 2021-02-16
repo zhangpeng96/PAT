74060ff34891d7bf69d53a2a5cfb143de704584e
zhangpeng96 <sjiaomail@gmail.com>
2021-02-16 19:39:51 +0800

    a1142，引入邻接表降低判断点集是否任意两点均相邻，多次提交可通过

--- PAT-A/a1142-maximal-clique/python-a1142.py
+++ PAT-A/a1142-maximal-clique/python-a1142.py
@@ -3,7 +3,7 @@
     @version  : 21.0216
     @author   : zhangpeng96
     @time     : 40'00"
-    @accepted : p3 timeout
+    @accepted : all
 """
 
 from itertools import combinations
@@ -12,7 +12,7 @@ adj_list = {}
 
 def is_clique(vertex):
     for v1, v2 in combinations(vertex,2):
-        if v2 not in adj_list.get(v1, []):
+        if not adj_table[v1][v2]:
             return 'Not a Clique'
     shared = set.intersection(*map(
         lambda x: set( adj_list.get(x,[]) ), vertex
@@ -23,10 +23,13 @@ def is_clique(vertex):
 
 
 vertex_n, edge_n = map(int, input().split())
+adj_table = [ [ 0 for _ in range(vertex_n+1) ] for _ in range(vertex_n+1) ]
+
 for _ in range(edge_n):
     v1, v2 = map(int, input().split())
     adj_list[v1] = adj_list.get(v1, []) + [v2]
     adj_list[v2] = adj_list.get(v2, []) + [v1]
+    adj_table[v1][v2] = adj_table[v2][v1] = 1
 
 for _ in range(int(input())):
     clique = list( map(int, input().split()[1:]) )
