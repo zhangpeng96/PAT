5f7e628dc22e6446b194c357441d7b18f82e8bd1
zhangpeng96 <sjiaomail@gmail.com>
2021-02-18 22:33:33 +0800

    a1080，用readlines读取数据效率略提高，但仍超时，整体代码可能还存在优化空间

--- PAT-A/a1080-graduate-admission/python-a1080.py
+++ PAT-A/a1080-graduate-admission/python-a1080.py
@@ -1,3 +1,10 @@
+"""
+    @name     : a1089
+    @version  : 21.0218
+    @author   : zhangpeng96
+    @time     : 59'05"
+    @accepted : p4 timeout
+"""
 
 class Apply():
     def __init__(self, appid, s):
@@ -6,8 +13,8 @@ class Apply():
         self.final = (self.exam + self.inter) / 2
     def __eq__(self, other):
         return (self.exam == other.exam) and (self.final == other.final) and (self.inter == other.inter) and (self.choice == other.choice)
-    def __repr__(self):
-        return 'e:{} i:{} f:{} ({}) | '.format(self.exam, self.inter, self.final, ' '.join(map(str, self.choice)))
+    # def __repr__(self):
+    #     return 'e:{} i:{} f:{} ({}) | '.format(self.exam, self.inter, self.final, ' '.join(map(str, self.choice)))
 
 class School():
     def __init__(self, quota):
@@ -18,8 +25,8 @@ class School():
     def accept(self, appid):
         self.quota -= 1
         self.apply.append(appid)
-    def __repr__(self):
-        return 'Q:{} ({}) | '.format(self.quota, ' '.join(map(str, self.apply)))
+    # def __repr__(self):
+    #     return 'Q:{} ({}) | '.format(self.quota, ' '.join(map(str, self.apply)))
 
 
 apply_n, school_n, choice_n = map(int, input().split())
@@ -28,8 +35,6 @@ school = [ School(int(x)) for x in input().split() ]
 applies = [ Apply(i, input()) for i in range(apply_n) ]
 applies.sort(key=lambda x:(x.final, x.exam), reverse=True)
 
-# print(applies)
-
 for i, app in enumerate(applies):
     for choice in app.choice:
         if school[choice]:
@@ -37,10 +42,8 @@ for i, app in enumerate(applies):
             same_rank_choice = choice
             break
         else:
-            # print(app, applies[i-1])
             if app == applies[i-1]:
                 school[same_rank_choice].accept(app.appid)
-                # print('eq')
                 break
 
 for sch in school:
