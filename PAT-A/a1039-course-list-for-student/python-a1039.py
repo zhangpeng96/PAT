875a5859b513620c1e361670882a9ad0a3cca28d
zhangpeng96 <sjiaomail@gmail.com>
2021-02-19 16:52:03 +0800

    a1039，p1问题解决，p5多次提交可通过

--- PAT-A/a1039-course-list-for-student/python-a1039.py
+++ PAT-A/a1039-course-list-for-student/python-a1039.py
@@ -3,7 +3,7 @@
     @version  : 21.0219
     @author   : zhangpeng96
     @time     : 38'00"
-    @accepted : p1 exception
+    @accepted : all
 """
 
 from collections import defaultdict
@@ -12,7 +12,9 @@ record = defaultdict(list)
 
 student_n, course_n = map(int, input().split())
 for _ in range(course_n):
-    course_id = int( input().split()[0] )
+    course_id, count = map(int, input().split())
+    # 可能存在有某个课程没有学生选，那么就没有少了一行输入，需要判断跳过
+    if count == 0: continue
     for name in input().split():
         record[name].append(course_id)
 
