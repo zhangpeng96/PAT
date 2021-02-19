f99ff7756f5dc7ac178d40168b3edc5b9aeaae4f
zhangpeng96 <sjiaomail@gmail.com>
2021-02-19 21:42:27 +0800

    a1106，优化后提高效率，但若干测试点内存超限

--- PAT-A/a1106-lowest-price-in-supply-chain/python-a1106.py
+++ PAT-A/a1106-lowest-price-in-supply-chain/python-a1106.py
@@ -3,13 +3,13 @@
     @version  : 21.0219
     @author   : zhangpeng96
     @time     : 25'00"
-    @accepted : p2,p3,p5,p6,p7 timeout
+    @accepted : p3,p6,p7 segment fault
 """
 
 import sys
 from collections import defaultdict
 
-sys.setrecursionlimit(20000)
+sys.setrecursionlimit(100000)
 
 def dfs(root, nodes, depth):
     if not nodes[root]:
@@ -25,8 +25,8 @@ count, price, rate = int(count), float(price), 1+(float(rate)/100)
 nodes = []
 retailer = defaultdict(int)
 
-for i in range(count):
-    nodes.append( list(map(int, input().split()[1:])) )
+for line in sys.stdin.readlines():
+    nodes.append( list(map(int, line.split()[1:])) )
 
 dfs(0, nodes, 0)
 
