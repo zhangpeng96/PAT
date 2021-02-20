27f21271d82ad1c49846efee7888c0181eba4809
zhangpeng96 <sjiaomail@gmail.com>
2021-02-20 22:35:29 +0800

    a1032，用列表不用类，效率可提高

--- PAT-A/a1032-sharing/python-a1032.py
+++ PAT-A/a1032-sharing/python-a1032.py
@@ -3,34 +3,29 @@
     @version  : 21.0220
     @author   : zhangpeng96
     @time     : 33'00"
-    @accepted : p5 timeout
+    @accepted : all
 """
 
 from sys import stdin
-
-class Node():
-    def __init__(self, rear):
-        self.next = rear
-        self.visited = False
 
 data = {}
 no_shared = True
 
 head1, head2, count = input().split()
-for _ in range(int(count)):
-    head, _, rear = input().split()
-    data[head] = Node(rear)
+for line in stdin.readlines():
+    head, _, rear = line.split()
+    data[head] = [rear, False]
 
 while head1 != '-1':
-    data[head1].visited = True
-    head1 = data[head1].next
+    data[head1][1] = True
+    head1 = data[head1][0]
 
 while head2 != '-1':
-    if data[head2].visited:
+    if data[head2][1]:
         print(head2)
         no_shared = False
         break
-    head2 = data[head2].next
+    head2 = data[head2][0]
 
 if no_shared:
     print('-1')
