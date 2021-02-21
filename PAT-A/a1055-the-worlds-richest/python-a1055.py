310f41b68cc8664069f18aafee198f598db18874
zhangpeng96 <sjiaomail@gmail.com>
2021-02-21 13:02:17 +0800

    a1055，可以先排序，再判断年龄输出，但仍超时

--- PAT-A/a1055-the-worlds-richest/python-a1055.py
+++ PAT-A/a1055-the-worlds-richest/python-a1055.py
@@ -17,15 +17,17 @@ for line in lines[:n]:
     name, age, worth = line.split()
     record.append([name, int(age), int(worth)])
 
+record.sort(key=lambda x: (-x[2], x[1], x[0]))
+
 for i, query in enumerate(lines[n:], 1):
+    valid = 0
     count, start, end = map(int, query.split())
-    result = sorted(
-        filter(lambda x: start <= x[1] <= end, record),
-        key=lambda x: (-x[2], x[1], x[0])
-    )
     print('Case #{}:'.format(i))
-    if result:
-        for res in result[:count]:
-            print('{} {} {}'.format(*res))
-    else:
+    for i in range(n):
+        if start <= record[i][1] <= end:
+            print(*record[i])
+            valid += 1
+            if valid == count:
+                break
+    if not valid:
         print('None')
