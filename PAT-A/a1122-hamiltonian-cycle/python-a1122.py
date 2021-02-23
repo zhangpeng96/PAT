6fd29e133502d70edfa737d7c7a848711f067d1d
zhangpeng96 <sjiaomail@gmail.com>
2021-02-23 18:46:21 +0800

    a1122，p2错误

---
+++ PAT-A/a1122-hamiltonian-cycle/python-a1122.py
@@ -0,0 +1,31 @@
+"""
+    @name     : a1122
+    @version  : 21.0223
+    @author   : zhangpeng96
+    @time     : 22'00"
+    @accepted : p2 error
+"""
+
+def judge(n, arr, vertex):
+    if arr[0] != arr[-1] or n != (vertex+1):
+        return False
+    for v1, v2 in zip(arr[:-1], arr[1:]):
+        if not graph[v1][v2]: return False
+    return True
+
+
+vertex_n, edge_n = map(int, input().split())
+
+graph = [ [False]*(vertex_n+1) for _ in range(vertex_n+1) ]  
+
+for _ in range(edge_n):
+    a, b = map(int, input().split())
+    graph[a][b] = True
+    graph[b][a] = True
+
+for _ in range(int(input())):
+    n, *arr = map(int, input().split())
+    if judge(n, arr, vertex_n):
+        print('YES')
+    else:
+        print('NO')
