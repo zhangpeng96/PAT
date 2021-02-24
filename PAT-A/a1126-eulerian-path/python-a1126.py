e44209d9b246ebd610fcdf77082e0771c9355b28
zhangpeng96 <sjiaomail@gmail.com>
2021-02-24 17:15:51 +0800

    a1126，仍不通过

--- PAT-A/a1126-eulerian-path/python-a1126.py
+++ PAT-A/a1126-eulerian-path/python-a1126.py
@@ -6,36 +6,39 @@
     @accepted : p4 error, p6 timeout
 """
 
-from collections import Counter
+from collections import Counter, defaultdict
 
-count = 0
-visited, adjacency = {}, {}
+def dfs(v1):
+    visited[v1] = True
+    count['all'] += 1
+    for v2 in range(1, vert+1):
+        if not visited[v2] and matrix[v1, v2]:
+            dfs(v2)
 
-def dfs(v):
-    visited[v] = True
-    global count
-    count += 1
-    for vert in adjacency.get(v, []):
-        if not visited.get(vert, False):
-            dfs(vert)
+
+count = {'all': 0, 'odd': 0}
+visited, matrix, degrees = defaultdict(bool), defaultdict(bool), defaultdict(int)
 
 vert, edge = map(int, input().split())
 for _ in range(edge):
     v1, v2 = map(int, input().split())
-    adjacency[v1] = adjacency.get(v1, []) + [v2]
-    adjacency[v2] = adjacency.get(v2, []) + [v1]
+    matrix[v1, v2] = True
+    matrix[v2, v1] = True
+    degrees[v1] += 1
+    degrees[v2] += 1
 
 dfs(1)
 
-degree = list(map( lambda x:len(x[1]), sorted(adjacency.items(), key=lambda x: x[0]) ))
-degree_odd = [ d % 2 for d in degree ]
-amount = Counter(degree_odd)
+for degree in degrees.values():
+    if degree % 2:
+        count['odd'] += 1
 
-print(' '.join(map(str, degree)))
-if count == vert:
-    if amount.get(0, 0) == vert:
+print(*map(lambda x: x[1], sorted(degrees.items())))
+
+if count['all'] == vert:
+    if count['odd'] == 0:
         print('Eulerian')
-    elif amount.get(1, 0) == 2 and amount.get(0, 0) == (vert-2):
+    elif count['odd'] == 2:
         print('Semi-Eulerian')
     else:
         print('Non-Eulerian')
