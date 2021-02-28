158d06641280800c0fdef4c61e227dd17098d5f5
zhangpeng96 <sjiaomail@gmail.com>
2021-02-28 21:57:00 +0800

    a1013，换用二维列表，p4超时

--- PAT-A/a1013-battle-over-cities/python-a1013.py
+++ PAT-A/a1013-battle-over-cities/python-a1013.py
@@ -1,25 +1,23 @@
 """
     @name     : a1013
-    @version  : 21.0228
+    @version  : 21.0228.2
     @author   : zhangpeng96
     @time     : 45'00"
-    @accepted : p2, p4 timeout
+    @accepted : p4 timeout
 """
 
 import sys
-from collections import defaultdict
-
 sys.setrecursionlimit(8000)
 
 def dfs(v1, visited):
     visited[v1] = True
-    for v2 in range(1,cities+1):
-        if not visited[v2] and graph[v1, v2]:
+    for v2 in range(1,cities):
+        if not visited[v2] and graph[v1][v2]:
             dfs(v2, visited)
 
 def find_component(nodes, pre_visited):
     count = 0
-    visited = defaultdict(bool)
+    visited = [False]*cities
     visited[pre_visited] = True
     for node in nodes:
         if not visited[node]:
@@ -29,14 +27,14 @@ def find_component(nodes, pre_visited):
 
 
 cities, highways, checked = map(int, input().split())
-graph = defaultdict(bool)
+cities += 1
+graph = [ [False]*cities for _ in range(cities) ]
 
 for _ in range(highways):
     city1, city2 = map(int, input().split())
-    graph[city1, city2] = True
-    graph[city2, city1] = True
-
+    graph[city1][city2] = True
+    graph[city2][city1] = True
 
 for city in map(int, input().split()):
-    component_count = find_component(range(1,cities+1), city)
+    component_count = find_component(range(1,cities), city)
     print(component_count-1)
