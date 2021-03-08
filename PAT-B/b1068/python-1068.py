145b882da5915cdf577e15312f6ea85a708c0fd5
zhangpeng96 <sjiaomail@gmail.com>
2021-03-08 16:58:56 +0800

    b1068，仍一个点超时

--- PAT-B/b1068/python-1068.py
+++ PAT-B/b1068/python-1068.py
@@ -1,97 +1,42 @@
-'''
-    @name      : b1068
-    @version   : 20.0526.3
-    @author    : zhangpeng96
-    @test_time : 122'00"
-    @pass_rate : p4 timeout
-'''
+"""
+    @name     : b1068
+    @version  : 21.0308
+    @author   : zhangpeng96
+    @time     : 40'00"
+    @accepted : p4 timeout
+"""
 
-def check_bit(bit, checks, gap):
-    return all( map(lambda n: abs(bit - n) > gap, checks) )
+# from pprint import pprint as print
+from collections import Counter
 
-def search_bit(bitmap, x, y, right, bottom, gap):
-    checks = []
-    if x == 0 and y == 0:
-        checks += [ bitmap[x][y+1], bitmap[x+1][y+1], bitmap[x+1][y] ]
-    elif x == 0 and 0 < y < right:
-        checks += [ bitmap[x][y+1], bitmap[x+1][y+1], bitmap[x+1][y], bitmap[x+1][y-1], bitmap[x][y-1] ]
-    elif x == 0 and y == right:
-        checks += [ bitmap[x+1][y], bitmap[x+1][y-1], bitmap[x][y-1] ]
-    elif 0 < x < bottom and y == right:
-        checks += [ bitmap[x+1][y], bitmap[x+1][y-1], bitmap[x][y-1], bitmap[x-1][y-1], bitmap[x-1][y] ]
-    elif x == bottom and y == right:
-        checks += [ bitmap[x][y-1], bitmap[x-1][y-1], bitmap[x-1][y] ]
-    elif x == bottom and  0 < y < right:
-        checks += [ bitmap[x-1][y], bitmap[x-1][y+1], bitmap[x][y+1], bitmap[x][y-1], bitmap[x-1][y-1] ]
-    elif x == bottom and y == 0:
-        checks += [ bitmap[x-1][y], bitmap[x-1][y+1], bitmap[x][y+1] ]
-    elif 0 < x < bottom and y == 0:
-        checks += [ bitmap[x-1][y], bitmap[x-1][y+1], bitmap[x][y+1], bitmap[x+1][y+1], bitmap[x+1][y] ]
-    else:
-        checks += [ bitmap[x-1][y], bitmap[x-1][y+1], bitmap[x][y+1], bitmap[x+1][y+1], bitmap[x+1][y], bitmap[x+1][y-1], bitmap[x][y-1], bitmap[x-1][y-1]]
-    return check_bit(bitmap[x][y], checks, gap)
-
-def search_bit2(bitmap, x, y, right, bottom, gap):
-    checks = []
-    if right == 0:
-        if x == 0:
-            checks += [ bitmap[x+1][y] ]
-        elif x == bottom:
-            checks += [ bitmap[x-1][y] ]
-        else:
-            checks += [ bitmap[x-1][y] + bitmap[x+1][y] ]
-    elif bottom == 0:
-        if y == 0:
-            checks += [ bitmap[x][y+1] ]
-        elif y == right:
-            checks += [ bitmap[x][y-1] ]
-        else:
-            checks += [ bitmap[x][y-1] + bitmap[x][y+1] ]
-    return check_bit(bitmap[x][y], checks, gap)
-
-def single_element(iters, bitmap_line):
-    single = []
-    for it in iters:
-        if bitmap_line.count(it) == 1:
-            single.append(it)
-    return single
+def check_bit(i, j):
+    checked = bitmap[i][j]
+    clockwise = [ (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0) ]
+    for side_x, side_y in clockwise:
+        x, y = i + side_x, j + side_y
+        if (x >= 0 and x < width and y >=0 and y < height) and abs(bitmap[x][y]-checked) <= threshold:
+            return False
+    return True
 
 
-width, height, gap = tuple(map(int, input().split()))
+width, height, threshold = map(int, input().split())
 bitmap = [ list( map(int, input().split()) ) for _ in range(height) ]
+counter = Counter(sum(bitmap, []))
+bitmap = tuple(zip(*bitmap))
 
-# width, height, gap = map(int, '2 1 100'.split())
-# inputs = ['199 33']
-# bitmap = [ list( map(int, i.split()) ) for i in inputs ]
-bitmap_line = sum(bitmap, [])
+count = 0
+result = tuple()
 
-right, bottom = width-1, height-1
-unique, option = {}, []
+for i in range(width):
+    for j in range(height):
+        bit = bitmap[i][j]
+        if counter[bit] == 1 and check_bit(i, j):
+            result = (i+1, j+1, bitmap[i][j])
+            count += 1
 
-if width == 1 and height == 1:
-    option.append(bitmap[0][0])
-    unique.update({ bitmap[0][0]: (1, 1)})
-
-elif width == 1 or height == 1:
-    for x in range(height):
-        for y in range(width):
-            if search_bit2(bitmap, x, y, right, bottom, gap):
-                bits = bitmap[x][y]
-                option.append(bits)
-                unique.update({ bits: (y+1,x+1) })
-else:
-    for x in range(height):
-        for y in range(width):
-            if search_bit(bitmap, x, y, right, bottom, gap):
-                bits = bitmap[x][y]
-                option.append(bits)
-                unique.update({ bits: (y+1,x+1) })
-
-option = single_element(option, bitmap_line)
-
-if len(option) == 1:
-    print('({}, {}): {}'.format(*unique[option[0]], option[0]))
-elif len(option) > 1:
+if count == 1:
+    print('({}, {}): {}'.format(*result))
+elif count > 1:
     print('Not Unique')
-elif len(option) == 0:
+else:
     print('Not Exist')
